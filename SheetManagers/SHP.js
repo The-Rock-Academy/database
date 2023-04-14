ec1d55d3d02ef30ad193f7d588101d5b04540fe0
James Thompson <1jamesthompson1@gmail.com>
2023-04-14 15:29:00 +1200

    Add-SHP-sheet-invoices (#1)

--- SheetManagers/SHP.js
+++ SheetManagers/SHP.js
@@ -1,5 +1,170 @@
 class SHPManager extends DatabaseSheetManager {
+    static sheetName() {
+        return "SHP";
+    }
+
+    static newFromSS(ss, currentTerm) {
+        return new SHPManager(ss.getSheetByName(SHPManager.sheetName()), currentTerm);
+    }
+
     constructor(sheet, currentTerm) {
         super(sheet, currentTerm);
+        this.currentInvoiceColumn = this.getColumn("Invoice", true)
     }
+
+    clean() {
+        console.log("Nothing to be cleaned yet on the SHP");
+    }
+
+    reset(nextTermDetails, nextTerm) {
+
+        // Insert new header and rows
+        this.sheet.insertRowsBefore(3, 31);
+        let headerRow = this.sheet.getRange(3,1,1,this.currentInvoiceColumn);
+
+        let finalTermWCDate = nextTermDetails[nextTermDetails.length - 1];
+        let tempDate = new Date(finalTermWCDate);
+        tempDate.setDate(tempDate.getDate() + 7);
+        let holidayStartDate = new Date(tempDate);
+        tempDate.setDate(tempDate.getDate() + 5);
+        let holidayEndDate = new Date(tempDate);
+        let sameMonth = holidayStartDate.getMonth() == holidayEndDate.getMonth();
+        
+        let headerText = "End of " + this.currentTerm + " : " +
+        holidayStartDate.getDate() + (sameMonth ? "" : " " + holidayStartDate.toLocaleDateString(undefined, { month: 'long'})) + " - "
+        + holidayEndDate.getDate() + " " + holidayEndDate.toLocaleDateString(undefined, { month: 'long'})
+        console.log(headerText);
+
+        headerRow.setValue(headerText).merge();
+    }
+
+    updateSheetAfterInvoiceSent(invoiceNumber, totalCost, sentDate) {
+        let invoiceRange = this.getInvoiceRanges(invoiceNumber);
+        invoiceRange.amount.setValue(totalCost);
+        invoiceRange.date.setValue(sentDate)
+        invoiceRange.paidDate.clearContent();
+    }
+
+    clearInvoiceNumber(invoiceNumber) {
+        console.log("Trying to clear " + invoiceNumber + " from the shp sheet")
+        try {
+          let invoiceInfo = this.getInvoiceRanges(invoiceNumber);
+    
+          //Dont clear the number if the user was trying to update the a invoice but decided against it.
+          if ((invoiceInfo.date.isBlank() && invoiceInfo.amount.isBlank())) {
+            invoiceInfo.number.clearContent();
+          }
+        }
+        catch(err) {
+          console.warn("You have tried to clear a invoice number that couldnt be found.\n" + err);
+        }
+    }
+
+    clearSheetAfterClearingInvoiceSender(invoiceNumber) {
+        this.clearInvoiceNumber(invoiceNumber);
+    }
+
+    prepareInvoice(row, send = false) {
+
+        let invoiceSheet = Invoices.newSheetManager(this, SpreadsheetApp.openById(this.databaseData.getVariable("Invoice Sender")).getSheetByName(this.databaseData.getVariable("Invoice Sender sheet name")));
+        let ui = SpreadsheetApp.getUi();
+        let activeRow = row;
+        
+        // --------------------------------
+        // Checking for previous invoices
+        // --------------------------------
+        // ---- pupilName -----
+        // Getting earlier as need to have this information for error message.
+        let pupilName = this.sheet.getRange(activeRow, this.getColumn("Student Name")).getValue();
+    
+        // Check if invoice has already been sent.
+        let invoiceNumberOfRow = this.getInvoiceNumberOfRow(activeRow)
+        let updating = false;
+        if (invoiceNumberOfRow != "" && !this.getInvoiceRanges(invoiceNumberOfRow).date.isBlank()) {
+            if (forTerm) {
+                let answer = ui.alert("It appears you have already made and sent an invoice for " + pupilName + " for the SHP.\nThe new invoice you create for this pupil will override the previous one you had.\nWould you like to continue with making a new one?", ui.ButtonSet.YES_NO)
+                if (answer == ui.Button.NO) {
+                    return
+                } else {
+                    updating = true
+                }
+            } else {
+            let invoiceInformation = this.getInvoiceRanges(this.getInvoiceNumberOfRow(row))
+            this.addTempInvoice(row, invoiceInformation.number.getValue(), !invoiceInformation.paidDate.isBlank())
+            invoiceInformation.number.clear()
+            }
+    
+        }
+    
+        // Check if the invoice sender is already occupied.
+        if (invoiceSheet.isInvoiceLoaded()) {
+            let answer = ui.alert("It appears the invoice sender already has an invoice loaded. Would you like to overide that invoice?", ui.ButtonSet.YES_NO)
+            if (answer == ui.Button.NO) {
+            return
+            } else {
+            invoiceSheet.clearInvoice();
+            }
+        }
+    
+        // --------------------------------
+        // Collecting information for invoice
+        // --------------------------------
+    
+        // ----- Get number of lessons -------
+    
+        //Find out what week to just assume all lessons will be attended
+        let price = this.sheet.getRange(activeRow, this.getColumn("Price")).getValue();
+    
+        let numberOfLessons = price == 350 ? 5 : price / 75;
+    
+        // ---- parentName -----
+        let parentName = this.sheet.getRange(activeRow, this.getColumn("Guardian")).getValue();
+    
+        let email = this.sheet.getRange(activeRow, this.getColumn("Email")).getValue();
+    
+        let instrumentHire = this.sheet.getRange(activeRow, this.getColumn("Instrument Hire cost")).getValue();
+    
+        let billingCompany = this.sheet.getRange(activeRow, this.getColumn("Pupils Billing Company")).getValue();
+    
+        if (!(parentName && email && billingCompany && pupilName && price && (numberOfLessons || numberOfLessons  == 0) &&this.currentTerm)) {
+            SpreadsheetApp.getUi().alert("Sorry the invoice for row " + row +" cannot be made as it is missing values. Please check all values and are present for the pupil.")
+            return;
+        }
+        // -----------------------------
+        // Create and load invoice into the invoice sheet
+        // -----------------------------
+        let invoice = Invoices.newInvoice(this.databaseData.getVariable("Invoice Folder"), parentName, pupilName, email, numberOfLessons, price / numberOfLessons, instrumentHire, billingCompany, "School holidays after " + this.currentTerm, "shp");
+        if (updating) {
+            invoice.number = this.getInvoiceNumberOfRow(row);
+            invoice.note = "This invoice is an updated version of an invoice sent on " + this.getInvoiceRanges(invoice.number).date.getValue().toLocaleString();
+        }
+        invoiceSheet.loadInvoice(invoice);
+    
+        // ----------------------------
+        // Load the invoice number into the attendance sheet
+        // ----------------------------
+        this.sheet.getRange(activeRow, this.currentInvoiceColumn).setValue(invoice.number);
+    
+        SpreadsheetApp.flush(); //This ensures that the invoice is actaully loaded at this point
+    
+        // -----------------------------
+        // Potentially send the invoice if needed
+        // -----------------------------
+        if(send) {
+            invoiceSheet.sendInvoice(updating);
+        }
+    }
+
+    /**
+     * Retrieves the invoice number of a row
+     * @param {number} rowNumber Row number you are checking
+     * @returns 
+    */
+    getInvoiceNumberOfRow(rowNumber) {
+        return this.sheet.getRange(rowNumber, this.currentInvoiceColumn).getValue()
+    }
+}
+
+function newSHPSheet(shpSheet) {
+    return new SHPManager(shpSheet)
 }
