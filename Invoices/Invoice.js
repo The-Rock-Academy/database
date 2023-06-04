3f5e219a94b60c9d4e7150c0f0f2ddccdf05724a
James <1jamesthompson1@gmail.com>
2023-06-04 17:51:43 +1200

    Updating project to new versions.
    Merging invoice in with this library

---
+++ Invoices/Invoice.js
@@ -0,0 +1,113 @@
+class Invoice {
+  constructor(ID, parentName, pupilName, email, lessons, trials, costPerLesson, instrumentHireCost, company, term, note = "", type) {
+    this.parentName = parentName;
+    this.pupilName = pupilName;
+    this.email = email;
+    this.lessons = lessons;
+    this.costPerLesson = costPerLesson;
+    this.instrumentHireCost = instrumentHireCost;
+    if (["TRA", "TSA", "GML"].includes(company)) {
+      this.company = company;
+    } else {
+      throw new Error("Cannot have billing company: " + company);
+    }
+    
+    //This is how the invoice numbers will be determined
+    this.number = ID;
+    this.term = term;
+    this.note = note;
+    // This should be one of three  values term, holiday and band.
+    this.type = type;
+    this.trials = trials;
+    this.updated = false; // Whether or not this invoice is an update of a previous invoice.
+  }
+
+  getCompanyInfo() {
+    switch (this.company) {
+      case 'TRA':
+        return {
+          name: "The Rock Academy",
+          image: "https://static.wixstatic.com/media/23b2e9_562405ec4e3344949bd3a22468d76d6c~mv2.png/v1/fill/w_553,h_202,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ROCK%20ACADEMY%20-%20Black%20on%20transparent_tif.png",
+          address: [["83 Ross Street, Kilbirnie"], ["Wellington, 6022"], ["021 565 750"]],
+          bankAccount: "01-0504-0147022-00"
+        }
+        break;
+      case 'TSA':
+        return {
+          name: "The Singing Academy",
+          image: "https://static.wixstatic.com/media/23b2e9_562405ec4e3344949bd3a22468d76d6c~mv2.png/v1/fill/w_553,h_202,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ROCK%20ACADEMY%20-%20Black%20on%20transparent_tif.png",
+          address: [["83 Ross Street, Kilbirnie"], ["Wellington, 6022"], ["021 565 750"]],
+          bankAccount: "06-0574-0245285-00"
+        }
+        break;
+      case 'GML':
+        return {
+          name: "Geoffs Music Lessons",
+          image: "https://static.wixstatic.com/media/23b2e9_562405ec4e3344949bd3a22468d76d6c~mv2.png/v1/fill/w_553,h_202,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/ROCK%20ACADEMY%20-%20Black%20on%20transparent_tif.png",
+          address: [["83 Ross Street, Kilbirnie"], ["Wellington, 6022"], ["021 565 750"]],
+          bankAccount: "06-0574-0800509-00"
+        }
+        break;
+      default:
+        throw("There is not a billing company for this invoice. Only managed to find '" + this.company + "' in the Database");
+    }
+  }
+
+/**
+ * This function will take the number of lessons and the name of pupils to produce a item object.
+ * This object will have desc quantity and price.
+ * It shall return an array of these objects
+ */
+  getCosts() {
+    let costs = [
+      {
+        desc: (this.pupilName + " music lesson"),
+        quantity: this.lessons,
+        price: this.costPerLesson
+      }
+    ]
+
+    if (this.instrumentHireCost != "") {
+      costs.push({
+        desc: ("Instrument hire"),
+        quantity: this.lessons + this.trials,
+        price: this.instrumentHireCost
+      })
+    }
+
+    return costs;
+  }
+
+  // /**
+  //  * Takes the first letter of a initial and turns it into a full string
+  //  */
+  // getInstrumentName() {
+  //   switch(this.instrumentHire) {
+  //     case 'P':
+  //       return "Piano";
+  //     case 'S':
+  //       return "Singing";
+  //     case 'B':
+  //       return "Bass";
+  //     case 'U':
+  //       return "Ukulele";
+  //     case 'G':
+  //       return "Guitar";
+  //   }
+  // }
+  // /**
+  //  * Returns how much the instrument hireage will be costing.
+  //  */
+  // getInstrumentCost() {
+  //   if (this.instrumentHire != "") {
+  //     return this.instrumentHire.split(" ").length * 12.50
+  //   } else {
+  //     return 0
+  //   }
+  // }
+}
+
+function newInvoice(invoiceFolderID, parentName, pupilName, email, lessons, trials, costPerLesson, instrumentHire, company, term, type) {
+  let ID = (new InvoiceFolder(invoiceFolderID)).getNumberOfInvoices() + 1;
+  return new Invoice(ID, parentName, pupilName, email, lessons, trials, costPerLesson, instrumentHire, company, term, "", type)
+}
---
+++ Invoices/InvoiceToSheet.js
@@ -0,0 +1,177 @@
+/**
+ * These classes here are designed to be the connections from the Invoice to the sheet.
+ * This means that the Invoice doesnt need to know anything about the Sheet and vice versa.
+ * 
+ * There is the super classs that is effectivly just a dictonary of ranges.
+ * Then the two sub classes are a cleaner and a loader.
+ * One of these takes all of the ranges and clears them filling some with place holders.
+ * The other one takes an invoice and loads them into all the needed ranges.
+ */
+
+class InvoiceSheetRangeManager {
+  constructor(sheet) {
+    this.sheet = sheet;
+  }
+
+  getParentRange() {
+    return this.sheet.getParent().getRangeByName("parents");
+  }
+
+  getEmail() {
+    return this.getParentRange().getValues()[1][0]
+  }
+
+  getParentName() {
+    return this.getParentRange().getValues()[0][0]
+  }
+
+  getLogoRange() {
+    return this.sheet.getParent().getRangeByName("logo")
+  }
+
+  getAddressRange() {
+    return this.sheet.getParent().getRangeByName("address")
+  }
+
+  getPaymentRange() {
+    return this.sheet.getParent().getRangeByName("payment")
+  }
+
+  getCostRange(itemNum, numberOfItems) {
+    let row = this.sheet.getParent().getRangeByName("firstCost").getRow()
+    return this.sheet.getRange(row - 1 + itemNum, 2,numberOfItems,5)
+  }
+
+  getCostsRange() {
+    return this.getCostRange(1,3)
+  }
+
+  getInvoiceNumberRange() {
+    return this.sheet.getParent().getRangeByName("invoiceNumber")
+  }
+
+  getTermRange() {
+    return this.sheet.getParent().getRangeByName("term")
+  }
+
+  getNotesRange() {
+    return this.sheet.getParent().getRangeByName("notes")
+  }
+
+  getDivisionMessageLocation() {
+    return this.sheet.getParent().getRangeByName("divisionMessage")
+  }
+
+  getTotalCostRange() {
+    return this.sheet.getParent().getRangeByName("totalCost")
+  }
+
+  getInvoiceInfoRange() {
+    return this.sheet.getParent().getRangeByName("invoiceInfo")
+  }
+
+}
+
+class InvoiceCleaner extends InvoiceSheetRangeManager {
+  constructor(sheet) {
+    super(sheet);
+  }
+
+  clean() {
+    //Clear all old things
+    super.getParentRange().clear({contentsOnly: true})
+    // super.getLogoRange().clear({contentsOnly: true})
+    super.getAddressRange().clear({contentsOnly: true})
+    super.getPaymentRange().clear({contentsOnly: true})
+    super.getCostsRange().clear({contentsOnly: true})
+    super.getInvoiceNumberRange().clear({contentsOnly: true})
+    super.getTermRange().clear({contentsOnly: true})
+    super.getNotesRange().clear({contentsOnly: true})
+    super.getDivisionMessageLocation().clear({contentsOnly: true})
+    super.getInvoiceInfoRange().clear({contentsOnly: true})
+
+    //Add in placeholders
+    // super.getLogoRange().setValue("Logo image here")
+    super.getParentRange().setValues([["parent name"], ["parents email"]])
+    super.getAddressRange().setValues([["Address 1"], ["Address 2"], ["Phone number"]])
+    super.getPaymentRange().setValues([["Company name"], ["Bank account"]])
+    super.getInvoiceNumberRange().setValue("invoice number")
+    super.getTermRange().setValue("term number and year")
+
+    // Delete all unused rows by calculating the total cost row.
+    let totalCostRow = super.getTotalCostRange().getRow();
+    let firstCostRow = super.getCostsRange().getRow();
+    let numberOfRows = totalCostRow - firstCostRow;
+    let extraRows = numberOfRows - 3;
+    if (extraRows > 0) {
+      this.sheet.deleteRows(firstCostRow + 3, extraRows);
+    }
+  }
+}
+
+class InvoiceLoader extends InvoiceSheetRangeManager{
+  constructor(sheet, invoice) {
+    super(sheet);
+    this.invoice = invoice;
+  }
+
+  loadInvoice(ssOfOrigin) {
+    debug("Loading invoice information into sheet")
+    this.loadParentInfo()
+    this.loadCosts()
+    this.loadInvoiceDetails()
+    this.loadCompanyInfo()
+    super.getNotesRange().setValue(this.invoice.note)
+    super.getInvoiceInfoRange().setValues([[ssOfOrigin.getId(), this.invoice.type, this.invoice.updated ? "update" : "new"]])
+    SpreadsheetApp.flush();
+    debug("Finished loading")
+  }
+
+  loadParentInfo() {
+    super.getParentRange().setValues([[this.invoice.parentName], [this.invoice.email]]);
+  }
+
+  /**
+   * This will load the compay information into the sheet
+   * There are 3 areas:
+   * logo
+   * address
+   * payment
+   */
+  loadCompanyInfo() {
+    let companyInfo = this.invoice.getCompanyInfo();
+
+    // Logo
+    //Disabled at the moment as all have the same image.
+    // let logo = SpreadsheetApp.newCellImage().setSourceUrl(companyInfo.image);
+    // super.getLogoRange().setValue(logo)
+
+    //Address
+    super.getAddressRange().setValues(companyInfo.address);
+
+    //payment
+    super.getPaymentRange().setValues([[companyInfo.name],[companyInfo.bankAccount]]);
+
+    if (companyInfo.name.localeCompare("The Rock Academy") != 0) {
+      super.getDivisionMessageLocation().setValue(companyInfo.name.concat(" - is a division of The Rock Academy"))
+    }
+  }
+
+  /**
+   * Currently only supports three cost items.
+   */
+  loadCosts() {
+    //Loop through all of the costs and add them in.
+    this.invoice.getCosts().forEach((cost, i) => {
+      super.getCostRange(1+i, 1).setValues([[cost.desc, , , cost.quantity, cost.price]])
+    })
+  }
+
+  loadInvoiceDetails() {
+    super.getInvoiceNumberRange().setValue(this.invoice.number);
+    super.getInvoiceNumberRange().setHorizontalAlignment("left");
+    super.getTermRange().setValue(this.invoice.term);
+  }
+}
+
+
